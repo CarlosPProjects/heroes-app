8a74bf78fa1319e4f7e3bd721f42420f164fddf8
CarlosPProjects <cg9822@gmail.com>
2024-12-13 23:06:36 +0100

    Add Error404 page component and update routing to handle 404 errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
