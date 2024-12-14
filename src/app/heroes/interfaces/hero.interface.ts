b124d0ae40ae5cff989637616246abab1907cf9c
CarlosPProjects <cg9822@gmail.com>
2024-12-14 19:51:24 +0100

    Refactor Hero Component and Layout: Update card component structure with Tailwind CSS styles, rename properties in Hero interface for consistency, and enhance layout page with a new main structure and title.

--- src/app/heroes/interfaces/hero.interface.ts
+++ src/app/heroes/interfaces/hero.interface.ts
@@ -2,8 +2,8 @@ export interface Hero {
   id: number;
   name: string;
   publisher: Publisher;
-  alterEgo: string;
-  firstAppearance: string;
+  alter_ego: string;
+  first_appearance: string;
   characters: String[];
   image?: string;
 }
